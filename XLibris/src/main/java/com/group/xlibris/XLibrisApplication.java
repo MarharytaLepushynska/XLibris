792a7e045d8442f2c3f76591febbf24b9993ee28
Marharyta Lepushynska <margo.lepush@gmail.com>
2026-09-13 18:25:17 +0300

    add project

---
+++ XLibris/src/main/java/com/group/xlibris/XLibrisApplication.java
@@ -0,0 +1,13 @@
+package com.group.xlibris;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XLibrisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XLibrisApplication.class, args);
+    }
+
+}
---
+++ XLibris/src/test/java/com/group/xlibris/XLibrisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group.xlibris;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XLibrisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
